36d55464389ea2aa12f00fba17aa72441cee196d
Rizki Kosasih <rizkikosasih1997@gmail.com>
2026-04-25 16:17:04 +0700

    feat(project): add IndexProjectRequest and use validated() instead of validate()

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,23 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Project\IndexProjectRequest;
 use App\Http\Requests\Project\StoreProjectRequest;
 use App\Http\Requests\Project\UpdateProjectRequest;
-use Illuminate\Http\Request;
 use App\Services\ProjectService;
 
 class ProjectController extends Controller
 {
     public function __construct(protected ProjectService $service) {}
 
-    public function index(Request $request)
+    public function index(IndexProjectRequest $request)
     {
-        return $this->service->list($request->only(['search', 'created_by']));
+        return $this->service->list($request->validated());
     }
 
     public function store(StoreProjectRequest $request)
     {
-        return $this->service->create($request->validate(), $request->user()->id);
+        return $this->service->create($request->validated(), $request->user()->id);
     }
 
     public function show($id)
@@ -28,7 +28,7 @@ class ProjectController extends Controller
 
     public function update(UpdateProjectRequest $request, $id)
     {
-        return $this->service->update($id, $request->validate());
+        return $this->service->update($id, $request->validated());
     }
 
     public function destroy($id)
