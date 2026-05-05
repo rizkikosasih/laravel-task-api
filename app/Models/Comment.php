03809e0e46591737dcd36419cf107be23ad92e5f
Rizki Kosasih <rizkikosasih1997@gmail.com>
2026-05-05 23:17:40 +0700

    chore(models): enable factory support

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, HasFactory;
 
     protected $fillable = ['task_id', 'user_id', 'message'];
 
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Enums\TaskStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Task extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, HasFactory;
 
     protected $fillable = [
         'project_id',
