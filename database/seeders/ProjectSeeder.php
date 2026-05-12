1f250741aee62a93c39d70d87f33941059342542
Rizki Kosasih <rizkikosasih1997@gmail.com>
2026-05-12 12:04:29 +0700

    feat: add created_by attribute to project seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -9,6 +9,7 @@ class ProjectSeeder extends Seeder
 {
     public function run(): void
     {
+        Project::factory(5)->create(['created_by' => 1]);
         Project::factory(5)->create();
     }
 }
